f2085d4c752a2bc9e3b080564371e4871f72f905
wongpakho814 <wongpakho814@gmail.com>
2023-01-15 15:57:57 +0800

    Implemented typeDefs.js
    Converting content in the routes and controllers directories to typeDefs.js and resolvers.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    _id: ID
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    getSingleUser(username: String!): User
+    getSingleUser(userId: ID!): User
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(userId: ID!, description: String!, bookId: String!, title: String!, image: String, link: String): User
+    removeBook(userId: ID!, bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
