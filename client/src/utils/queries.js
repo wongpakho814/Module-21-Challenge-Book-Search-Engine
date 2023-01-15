dc648f73c615ff082845f86ad7047bee2b661434
wongpakho814 <wongpakho814@gmail.com>
2023-01-15 17:44:17 +0800

    Implemented utils/queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,39 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      savedBooks {
+        _id
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        _id
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
